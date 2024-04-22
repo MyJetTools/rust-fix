b774f9fe733267d5bc86b256429bbebc2de287e7
amigin <amigin@gmail.com>
2024-04-22 12:18:20 +0300

    Removed Unused Traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
 mod errors;
 mod fix_message_builder;
-mod fix_serializer;
 mod utils;
 
 pub use errors::*;
 pub use fix_message_builder::*;
-pub use fix_serializer::{FixDeserializeModel, FixSerializeModel};
 pub use utils::*;
